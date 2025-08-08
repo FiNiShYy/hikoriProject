dad4942bb6e7e28c68c0226d674629da47120c5c
FiNiShYy <kaua@saltsystems.com.br>
2025-08-08 17:50:10 -0300

    Update styles in home.jsx for responsive design adjustments

--- src/styles/home.jsx
+++ src/styles/home.jsx
@@ -2,7 +2,7 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  padding: 80px 0;
+  padding: 80px 5vw;
   background-color: #1f1f1fef;
   min-height: 100vh;
   overflow-y: hidden;
@@ -10,6 +10,10 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
+
+  @media (max-width:480px) {
+    padding: 40px 2vw;
+  }
 `
 
 export const Title = styled.div`
@@ -27,6 +31,15 @@ export const Title = styled.div`
     margin: 0;
     line-height: 1.2;
   }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 28px;
+    padding: 10px;
+  }
 `
 
 export const CarrosselArea = styled.div`
@@ -36,11 +49,23 @@ export const CarrosselArea = styled.div`
   display: flex;
   justify-content: center;
   padding: 40px 0;
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `
 
 export const SpotArea = styled.div`
   z-index: 1;
   width: 700px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `
 
 export const Timer = styled.div`
@@ -56,6 +81,16 @@ export const Timer = styled.div`
     color: #fbfbfb;
     text-align: center;
   }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 20px;
+    padding: 20px 0;
+  }
+
 `
 
 export const DeclarationText = styled.div`
@@ -75,5 +110,17 @@ export const DeclarationText = styled.div`
     line-height: 1.5;
     color: #fbfbfb;
     text-align: justify;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 16px;
+    padding: 10px;
+    h1 {
+      font-size: 18px;
+    }
   }
 `
